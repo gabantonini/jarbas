248fcaf32de423c145e7fff63e83d416025ada52
victor albanezi <victor.albanezi@hotmail.com>
2020-05-05 20:40:59 -0300

    fixed users. new migration

--- db/migrate/20200505212011_change_zip_code_to_be_string_in_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeZipCodeToBeStringInUsers < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :zip_code, :string
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,14 @@ require 'open-uri'
 
 puts "creating users"
 20.times do 
-    file = URI.open("https://source.unsplash.com/collection/9300133/#{rand(1..18)}")
+    # file = URI.open("https://source.unsplash.com/collection/9300133/#{rand(1..18)}")
     user = User.new()
     user.first_name = Faker::Name.first_name  
     user.last_name =  Faker::Name.last_name 
     user.address = Faker::Address.street_name 
     user.zip_code = "12345-678"
     user.email = Faker::Internet.email
-    user.photo.attach(io: file, filename: 'photo.jpg', content_type: 'image/jpg')
+    # user.photo.attach(io: file, filename: 'photo.jpg', content_type: 'image/jpg')
     user.password = "123456"    
     user.save ? (puts "user saved") : (puts "invalid user: #{user.errors.full_messages}")
     puts "saved"
