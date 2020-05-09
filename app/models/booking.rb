546d332f36961ab8ae951e7fc8a5567fead168ce
Aurélien Jacomy <jacomya@gmail.com>
2020-05-09 11:37:02 -0300

    Added a review system to the my_bookings page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :service
   belongs_to :user
   has_one :review, dependent: :destroy
+
+  def add_review?
+  	(["Realizado", "Cancelado"].include?(status)) & (self.review.nil?)
+  end
 end
