08fdbdab349214f3df65827e2c95923e65bdaca5
Aurélien Jacomy <jacomya@gmail.com>
2020-05-09 12:14:17 -0300

    Alert banner for pending reviews

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,4 +6,9 @@ class Booking < ApplicationRecord
   def add_review?
   	(["Realizado", "Cancelado"].include?(status)) & (self.review.nil?)
   end
+  
+  def self.pending_reviews?(user)
+     my_bookings = Booking.where('user_id = ? AND status = ? AND date > ?', user.id, "Realizado", Date.today - 7)
+     my_bookings.count.zero? ? false : my_bookings.count
+  end
 end
