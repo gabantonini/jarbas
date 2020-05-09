baed0fe17a2d066ad6d2294eeb32ff1803078a21
Aurélien Jacomy <jacomya@gmail.com>
2020-05-09 11:14:10 -0300

    Link to new service and add service category to new form

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -37,6 +37,6 @@ class ServicesController < ApplicationController
   end
 
   def service_params
-  	params.require(:service).permit(:name, :description, :price, :time_to_answer, :disponibility, :photo)
+  	params.require(:service).permit(:name, :description, :price, :time_to_answer, :disponibility, :service_category_id, :photo)
   end
 end
