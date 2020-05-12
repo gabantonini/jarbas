933989d1546653fcd06f08b98a97630b9a81db2b
Aurélien Jacomy <jacomya@gmail.com>
2020-05-12 20:44:05 -0300

    Added date validation and set calendar automatically to first available date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :service
   belongs_to :user
   has_one :review, dependent: :destroy
-
-
+  validate :date_need_to_be_later_than_time_to_answer
+  
   def pending_confirmation?
     self.status == "Aguardando confirmação"
   end
@@ -27,6 +27,14 @@ class Booking < ApplicationRecord
       my_bookings_without_review << booking if booking.review.nil? 
      end
      my_bookings_without_review.count.zero? ? false : my_bookings_without_review.count
+  end
+
+  private 
+
+  def date_need_to_be_later_than_time_to_answer
+    if date < Date.today + self.service.time_to_answer
+      errors.add(:date, "O usuário solicitou ser avisado com #{self.service.time_to_answer} dia#{"s" if self.service.time_to_answer > 1} de antecedência")
+    end
   end
 
 end
