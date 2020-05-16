7c469bb471e9a5076d10ef9cc8c3cdb622b2aaab
Gabriel Antonini <gabriel.antonini@gmail.com>
2020-05-15 23:21:07 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_05_14_231246) do
-
+ActiveRecord::Schema.define(version: 2020_05_15_004455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +103,8 @@ ActiveRecord::Schema.define(version: 2020_05_14_231246) do
     t.string "address"
     t.string "zip_code"
     t.bigint "condominio_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["condominio_id"], name: "index_users_on_condominio_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
